8bcec5d3ea7ed3cf01cc50974407b5a8ae0dde92
mmnunes <martamnunes@gmail.com>
2016-11-09 22:18:42 +0000

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,70 +5,92 @@
 ## 4. Appropriately labels the data set with descriptive activity names.
 ## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-if (!require("data.table")) {
-  install.packages("data.table")
+## Download the dataset and unzip folder
+
+## Check if directory already exists
+if(!file.exists("./projectData")){
+  dir.create("./projectData")
 }
 
-if (!require("reshape2")) {
-  install.packages("reshape2")
+setwd("./projectData")
+
+Url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+zipfile <- "UCI HAR Dataset.zip"
+download.file(Url, zipfile, mode = "wb")
+
+if(file.exists(zipfile)) unzip(zipfile)
+
+
+## 1. Merges the training and the test sets to create one data set.
+## a. Read X_train, y_train data and subject_train.
+trainData <- read.table("./UCI HAR Dataset/train/X_train.txt", header= FALSE)
+trainLabel <- read.table("./UCI HAR Dataset/train/y_train.txt", header= FALSE)
+trainSubject <- read.table("./UCI HAR Dataset/train/subject_train.txt", header= FALSE)
+
+## b. Read X_test, y_test data and subject_test.
+testData <- read.table("./UCI HAR Dataset/test/X_test.txt", header= FALSE)
+testLabel <- read.table("./UCI HAR Dataset/test/y_test.txt", header= FALSE)
+testSubject <- read.table("./UCI HAR Dataset/test/subject_test.txt", header= FALSE)
+
+## c. Concatenate trainData/testData, trainLabel/testLabel and trainSubject/testSubject
+joinData <- rbind(trainData, testData)
+joinLabel <- rbind(trainLabel, testLabel)
+joinSubject <- rbind(trainSubject, testSubject)
+
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## a. Read features.txt
+features <- read.table("./UCI HAR Dataset/features.txt")
+
+## b.Extract measures mean and standard deviation
+columnsWithMeanSTD <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+joinData <- joinData[, columnsWithMeanSTD]
+
+## c. Clean column names and capitalize first letter of mean and std
+names(joinData) <- gsub("\\(\\)", "", features[columnsWithMeanSTD, 2])
+names(joinData) <- gsub("-", "", names(joinData)) 
+names(joinData) <- gsub("mean", "Mean", names(joinData)) 
+names(joinData) <- gsub("std", "Std", names(joinData)) 
+
+## 3. Uses descriptive activity names to name the activities in the data set
+## a. Read activity labels and Clean activity names in the second column
+activity <- read.table("./UCI HAR Dataset/activity_labels.txt")
+activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
+substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
+substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
+
+## b. Transform joinLabel according to activityLabel
+activityLabel <- activity[joinLabel[, 1], 2]
+joinLabel[, 1] <- activityLabel
+names(joinLabel) <- "activity"
+
+## 4. Appropriately labels the data set with descriptive activity names.
+names(joinSubject) <- "subject"
+cleanedData <- cbind(joinSubject, joinLabel, joinData)
+write.table(cleanedData, "tidydata.txt",row.names=FALSE,quote = FALSE, sep = '\t') 
+
+
+## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+subjectLength <- length(table(joinSubject))
+activityLength <- dim(activity)[1] 
+columnLength <- dim(cleanedData)[2]
+
+result <- matrix(NA, nrow=subjectLength*activityLength, ncol=columnLength) 
+result <- as.data.frame(result)
+
+colnames(result) <- colnames(cleanedData)
+row <- 1
+
+for(i in 1:subjectLength) {
+  for(j in 1:activityLength) {
+    result[row, 1] <- sort(unique(joinSubject)[, 1])[i]
+    result[row, 2] <- activity[j, 2]
+    flag1 <- i == cleanedData$subject
+    flag2 <- activity[j, 2] == cleanedData$activity
+    result[row, 3:columnLength] <- colMeans(cleanedData[flag1&flag2, 3:columnLength])
+    row <- row + 1
+  }
 }
+head(result)
 
-require("data.table")
-require("reshape2")
-
-
-# Load: activity labels and data column names
-activitylabels <- read.table("./UCI HAR Dataset/activity_labels.txt" )[,2]
-features <- read.table("./UCI HAR Dataset/features.txt")[,2]
-
-# Extract only the measurements on the mean and standard deviation for each measurement.
-columnsWithMeanSTD <- grepl("mean|std", features)
-
-# Load and process X_test and y_test data.
-activitytest <- read.table("./UCI HAR Dataset/test/X_test.txt", header= FALSE)
-featuretest <- read.table("./UCI HAR Dataset/test/y_test.txt", header= FALSE)
-subjecttest <- read.table("./UCI HAR Dataset/test/subject_test.txt", header= FALSE)
-
-names(activitytest) = features
-
-# Extract only the measurements on the mean and standard deviation for each measurement.
-activitytest = activitytest[,columnsWithMeanSTD]
-
-# Load activity labels
-featuretest[,2] = activitylabels[featuretest[,1]]
-names(featuretest) = c("Activity_ID", "Activity_Label")
-names(subjecttest) = "subject"
-
-# Bind data
-testdata <- cbind(as.data.table(subjecttest), featuretest, activitytest)
-
-# Load and process X_train & y_train data.
-activitytrain <- read.table("./UCI HAR Dataset/train/X_train.txt", header= FALSE)
-featuretrain <- read.table("./UCI HAR Dataset/train/y_train.txt", header= FALSE)
-subjecttrain <- read.table("./UCI HAR Dataset/train/subject_train.txt", header= FALSE)
-
-names(activitytrain) = features
-
-# Extract only the measurements on the mean and standard deviation for each measurement.
-activitytrain = activitytrain[,columnsWithMeanSTD]
-
-# Load activity labels
-featuretrain[,2] = activitylabels[featuretrain[,1]]
-names(featuretrain) = c("Activity_ID", "Activity_Label")
-names(subjecttrain) = "subject"
-
-# Bind data
-traindata <- cbind(as.data.table(subjecttrain), featuretrain, activitytrain)
-
-# Merges the training and the test sets to create one data set
-data = rbind(testdata, traindata)
-
-
-idlabels   = c("subject", "Activity_ID", "Activity_Label")
-datalabels = setdiff(colnames(data), idlabels)
-meltdata      = melt(data, id = idlabels, measure.vars = datalabels)
-
-# Apply mean to dataset using dcast function
-tidydata   = dcast(meltdata, subject + Activity_Label ~ variable, mean)
-
-write.table(tidydata, file = "./tidydata.txt",row.names = FALSE)
+write.table(result, "tidydatameans.txt",row.names=FALSE,quote = FALSE, sep = '\t') 
